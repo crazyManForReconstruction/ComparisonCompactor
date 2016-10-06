2be836d63d4645fdae58f3a1c6a2347277aef16e
crazyManForReconstruction <xuxiaoping1990@126.com>
2016-10-06 19:32:46 +0800

    Create TestComparisonCompactor.java
    用main函数测试ComparisonCompactor类

---
+++ TestComparisonCompactor.java
@@ -0,0 +1,7 @@
+
+public class TestComparisonCompactor {
+	public static void main(String[] args){
+		//ComparisonCompactor cc = new ComparisonCompactor(1, "abcd", "abcfd");
+		//System.out.println(cc.compact("bbb"));
+	}
+}
